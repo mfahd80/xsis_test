f806ee29c27fe668fa15f4d459e4c5189bd0ecc0
fahd-ait <muhammad.fahd@akarinti.tech>
2023-03-24 19:05:11 +0700

    initialize git

---
+++ src/main/java/com/example/xsis/XsisRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.xsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsisRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XsisRestApiApplication.class, args);
+	}
+
+}
